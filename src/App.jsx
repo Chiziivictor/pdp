9b81252d3e1b108d488983826923f4eea5abbd57
chiziivictor <chizivwokoma@gmail.com>
2022-04-04 23:06:25 +0100

    mobile responsive

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,24 @@ import swal from "sweetalert";
 
 const Container = styled.div`
   display: flex;
-  justify-content: center;
+  flex-direction: column;
+  justify-content: space-between;
   align-items: center;
+  min-height: 90vh;
+`;
+
+const Footer = styled.footer`
+  opacity: 0.5;
 `;
 const Wrapper = styled.div`
-  padding: 50px;
   display: flex;
+  padding: 50px;
+  padding-bottom: 20px;
+  display: flex;
+  @media (max-width: 768px) {
+    padding: 10px;
+    flex-direction: column;
+  }
 `;
 const ImgContainer = styled.div`
   flex: 1;
@@ -19,10 +31,17 @@ const Image = styled.img`
   width: 100%;
   height: 80vh;
   object-fit: contain;
+  @media (max-width: 768px) {
+    height: 40vh;
+  }
 `;
 const InfoContainer = styled.div`
   flex: 1;
   padding: 0px 50px;
+  @media (max-width: 768px) {
+    flex: 2;
+    align-items: center;
+  }
 `;
 const Title = styled.h1``;
 const Desc = styled.p`
@@ -46,6 +65,10 @@ const AddContainer = styled.div`
   align-items: center;
   justify-content: space-between;
   margin-top: 16px;
+  @media (max-width: 768px) {
+    width: auto;
+    justify-content: flex-start;
+  }
 `;
 
 const AmountContainer = styled.div`
@@ -77,6 +100,7 @@ const Amount = styled.span`
 `;
 
 const Button = styled.button`
+  margin-left: 10%;
   padding: 14px;
   border: 1px solid teal;
   background-color: white;
@@ -97,10 +121,14 @@ const Product = () => {
     return swal("Item added to Cart");
   };
 
+  const random = Math.floor(Math.random() * 20 + 1);
+
   useEffect(() => {
     setLoading(true);
     const getProduct = async () => {
-      const response = await fetch(`https://fakestoreapi.com/products/3`);
+      const response = await fetch(
+        `https://fakestoreapi.com/products/${random}`
+      );
       setProduct(await response.json());
       setLoading(false);
     };
@@ -119,7 +147,7 @@ const Product = () => {
           <InfoContainer>
             <Title>{product.title}</Title>
             <Desc>{product.description}</Desc>
-            <Price>{product.price}</Price>
+            <Price>$ {product.price}</Price>
             <AddContainer>
               <AmountContainer>
                 <Remove
@@ -139,6 +167,10 @@ const Product = () => {
           </InfoContainer>
         </Wrapper>
       )}
+      <Footer className="text-center text-muted">
+        {" "}
+        © {new Date().getFullYear()} Chizi Victor
+      </Footer>
     </Container>
   );
 };
